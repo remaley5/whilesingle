e3901f800cd3c9b6a9ccbe1887c4eb98ddecae38
Dillon Long <dplong@crimson.ua.edu>
2020-10-26 09:51:17 -0700

    Added basic stuff for login-signup page. Also rearranged how App.js and index.js are structured to all the use of location in App.js

--- starter/client/src/App.js
+++ starter/client/src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink, useLocation } from 'react-router-dom';
 
 import UserList from './components/UsersList';
-
+import LoginSignup from './components/LoginSignup';
 
 function App() {
+  let location = useLocation();
 
   return (
-    <BrowserRouter>
+    <>
+        {location.pathname !== '/login-signup' ?
         <nav>
             <ul>
                 <li><NavLink to="/" activeclass="active">Home</NavLink></li>
                 <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                <li><NavLink to="/login-signup" activeclass="active">Login/Signup</NavLink></li>
             </ul>
-        </nav>
+        </nav> : null}
         <Switch>
             <Route path="/users">
                 <UserList />
+            </Route>
+
+            <Route path="/login-signup">
+                <LoginSignup />
             </Route>
 
             <Route path="/">
                 <h1>My Home Page</h1>
             </Route>
         </Switch>
-    </BrowserRouter>
+    </>
   );
 }
 
---
+++ starter/client/src/components/LoginSignup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function LoginSignup(props) {
+    return (
+        <div>
+          <div>Logo</div>
+          <div>
+            <div>Have an account?</div>
+            <button>Sign in</button>
+          </div>
+          <h1>Dating Deserves Better</h1>
+          <p>On OkCupid, you’re more than just a photo. You have stories to tell, and passions to share, and things to talk about that are more interesting than the weather. Get noticed for who you are, not what you look like. Because you deserve what dating deserves: better.</p>
+          <button>Join OKCUPID</button>
+        </div>
+    );
+}
+export default LoginSignup;
