4022e88628442816950196c605bace5b616576e5
Isaac <finken.isaac@gmail.com>
2020-10-26 18:10:31 -0500

    Starting on redux store; revised match routes.

--- starter/starter_app/api/match_routes.py
+++ starter/starter_app/api/match_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, url_for, request, jsonify
 from ..models import (db, User, Match)
-# from sqlalchemy import and_
+# from sqlalchemy import notin_
 from pprint import pprint
 import itertools
 
@@ -11,20 +11,23 @@ current_user_id = 2
 
 # Get all users except current:
 # -------------------------
-# @user_routes.route("/swipe")
-# def get_users():
-#     newFriends = User.query.order_by(User.id)
-#     user_list = [{'id': f.id, 'username': f.username} for f in newFriends if f.id != user_id]
-#     return jsonify(user_list)
-
+@match_routes.route("/swipe/<user_id_param>")
+def get_users(user_id_param):
+    user_id = int(user_id_param)
+    user_subquery = User.query.filter(id == user_id).first().subquery()
+    matches = User.query.join(User.matches).filter(
+        user_subquery.notin_(Match.users)).all()
+    # matches = list(itertools.chain(*[[{'id': m.id, 'username': m.username} for m in f.users if m.id != current_user_id] for f in matches]))
+reuthturn ""this did something!
 
 # Gets all your existing matches
 # -------------------------------
 @match_routes.route("/get-matches")
 def get_matches():
-    matches = Match.query.join(Match.users).filter(User.id == current_user_id).all()
-    matches = list(itertools.chain(*[[{'id': m.id, 'username': m.username} for m in f.users if m.id != current_user_id] for f in matches]))
-    return jsonify(matches)
+    matches = Match.query.join(Match.users).filter(
+        User.id == current_user_id).all()
+    matches = list(itertools.chain(*[[{'id': m.id, 'username': m.username}
+                                      for m in f.users if m.id != current_user_id] for f in matches]))
 
 
 # Creates a match
@@ -35,7 +38,6 @@ def add_match():
     friend_id = request.args.get('user_id')
     users = User.query.filter(User.id.in_([current_user_id, friend_id])).all()
     newMatch = Match()
-    newMatch.users.extend(users)
     db.session.add(newMatch)
     db.session.commit()
     return jsonify({'match_id': newMatch.id})
