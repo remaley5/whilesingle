997b554822e1a77e449f9e905b17f82af9e67cda
Miah Barnes <miahellenbarnes@gmail.com>
2020-10-26 16:48:47 -0400

    stuff

--- starter/client/src/components/messengerComponents/Messages.js
+++ starter/client/src/components/messengerComponents/Messages.js
@@ -6,6 +6,8 @@ import TextField from '@material-ui/core/TextField'
 const Messages = () => {
     const [users, setUsers] = useState([]);
     const [selectedName, setSelectedName] = useState();
+    const [recipientId, setRecipientId] = useState();
+    const [message, setMessage] = useState();
 
     useEffect(() => {
         async function fetchData() {
@@ -17,13 +19,17 @@ const Messages = () => {
     }, []);
 
     const handleClick = (e) => {
-        const username = e.target.innerHTML
-        setSelectedName(username)
-        console.log(e.target.value)
+        setSelectedName(e.target.innerHTML)
+        setRecipientId(e.target.getAttribute('value'))
         // console.log(selectedName)
     }
 
-    const userComponents = users.map((user) => <div className='user' value='hey' key={user.id} onClick={handleClick}>{user.username}</div>)
+    const handleSend = (e) => {
+        e.preventDefault()
+        console.log(message)
+    }
+    const handleMessage = (e) => setMessage(e.target.value)
+    const userComponents = users.map((user) => <div className='user' value={user.id} key={user.id} onClick={handleClick}>{user.username}</div>)
     return (
         <div className='messenger'>
             <div className='side-bar'>
@@ -31,10 +37,10 @@ const Messages = () => {
             </div>
             <div className='compose-message'>
                 <div>Hey BOI</div>
-                <form className='message-box'>
+                <form className='message-box' onSubmit={handleSend}>
                     <TextField className='message-sender'
-                    id="filled-basic" label={!selectedName ?"Talk to them!" : `Talk to ${selectedName}`}
-                    variant="filled" />
+                    rowsMax={3} onChange={handleMessage} size='medium'
+                    id="standard-basic" label={!selectedName ?"Talk to them!" : `Talk to ${selectedName}`} />
                 </form>
             </div>
         </div>
