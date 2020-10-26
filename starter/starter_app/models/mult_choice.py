48fbf61865daa8ea189740f3d10a445da20d15cb
Isaac <finken.isaac@gmail.com>
2020-10-26 11:02:28 -0500

    Added basic free response models and one question.

--- starter/starter_app/models/mult_choice.py
+++ starter/starter_app/models/mult_choice.py
@@ -6,34 +6,38 @@ class MC_Response(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    user_id = db.Column(db.ForeignKey('users.id', ondelete='cascade'), nullable=False)
+    user_id = db.Column(db.ForeignKey(
+        'users.id', ondelete='cascade'), nullable=False)
 
-    mc_answer_option_id = db.Column(db.ForeignKey('mc_answer_options.id', ondelete='cascade'), nullable=False)
+    mc_answer_option_id = db.Column(db.ForeignKey(
+        'mc_answer_options.id', ondelete='cascade'), nullable=False)
 
     mc_answer_option = db.relationship('MC_Answer_Option')
 
-    mc_question_id = db.Column(db.ForeignKey('mc_questions.id', ondelete='cascade'), nullable=False)
+    mc_question_id = db.Column(db.ForeignKey(
+        'mc_questions.id', ondelete='cascade'), nullable=False)
 
-    mc_question = db.relationship('MC_Question')
+    mc_questions = db.relationship('MC_Question')
 
 
 class MC_Question(db.Model):
-    pass
     __tablename__ = 'mc_questions'
 
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Text, nullable=False)
 
-    mc_answer_options = db.relationship('MC_Answer_Option', back_populates="mc_question")
+    mc_answer_options = db.relationship(
+        'MC_Answer_Option', back_populates="mc_questions")
 
 
 class MC_Answer_Option(db.Model):
-    pass
     __tablename__ = 'mc_answer_options'
 
     id = db.Column(db.Integer, primary_key=True)
     answer = db.Column(db.Text, nullable=False)
 
-    mc_question_id = db.Column(db.ForeignKey('mc_questions.id', ondelete='cascade'), nullable=False)
+    mc_question_id = db.Column(db.ForeignKey(
+        'mc_questions.id', ondelete='cascade'), nullable=False)
 
-    mc_question = db.relationship('MC_Question', back_populates='mc_answer_options')
+    mc_questions = db.relationship(
+        'MC_Question', back_populates='mc_answer_options')
