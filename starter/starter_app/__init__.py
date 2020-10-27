c614ca6bbc7198637edd40a26a6f3ff1eb05b20d
Dillon Long <dplong@crimson.ua.edu>
2020-10-27 09:13:16 -0700

    Added flask_login features to __init__.py file. Added properties to User model to work with login features.

--- starter/starter_app/__init__.py
+++ starter/starter_app/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request, session
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_migrate import Migrate
+from flask_login import LoginManager, current_user, login_user, logout_user, \
+                        login_required
 
-
-from starter_app.models import db, User, MC_Response, MC_Question, MC_Answer_Option
+from starter_app.models import db, User, MC_Response, MC_Question, \
+                               MC_Answer_Option
 
 from starter_app.api.user_routes import user_routes
 
@@ -16,21 +18,20 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
+login_manager = LoginManager(app)
 
 # included so alembic migrations folder within models folder
 MIGRATION_DIR = os.path.join('starter_app', 'models', 'migrations')
 Migrate(app, db, directory=MIGRATION_DIR)
 
-## Application Security
+# Application Security
 CORS(app)
-@app.after_request
-def inject_csrf_token(response):
-    response.set_cookie('csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') else False,
-        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
-        httponly=True)
-    return response
+CSRFProtect(app)
+
+
+@login_manager.user_loader()
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.route('/', defaults={'path': ''})
@@ -40,3 +41,36 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+
+@app.route('/api/csrf/restore')
+def restore_csrf():
+    return {'csrf_token': generate_csrf()}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if not username or not password:
+        return {"errors": ["Missing required parameters"]}, 400
+
+    authenticated, user = User.authenticate(username, password)
+    # print(authenticated)
+    # print(user)
+    if authenticated:
+        login_user(user)
+        return {"current_user_id": current_user.id}
+
+    return {"errors": ["Invalid username or password"]}, 401
+
+
+@app.route('logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return {"msg": "You have been logged out"}, 200
--- starter/starter_app/models/user.py
+++ starter/starter_app/models/user.py
@@ -1,7 +1,6 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 class User(db.Model):
@@ -10,6 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    password_digest = db.Column(db.String(255), nullable=False)
 
     mc_responses = db.relationship('MC_Response', backref='users')
 
@@ -19,3 +19,16 @@ class User(db.Model):
             "username": self.username,
             "email": self.email
         }
+
+    @property
+    def password(self):
+        raise AttributeError('Password not readable.')
+
+    @password.setter
+    def password(self, password):
+        self.password_digest = generate_password_hash(password)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        user = cls.query.filter(User.username == username).scalar()
+        return check_password_hash(user.password_digest, password), user
