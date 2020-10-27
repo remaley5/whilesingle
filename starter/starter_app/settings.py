705b54fb4c544b3cc831d43155b8b428af94429a
Sophie_Willson <swillson@alumni.risd.edu>
2020-10-27 15:20:29 -0700

    pipfile

---
+++ starter/starter_app/settings.py
@@ -0,0 +1,11 @@
+from .config import Configuration
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'storages',
+]
