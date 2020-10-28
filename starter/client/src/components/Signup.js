cfc33028aa0ed9db988aaab09a5fb342698a1ab8
Dillon Long <dplong@crimson.ua.edu>
2020-10-28 15:25:24 -0700

    Added frontend for basic signup page.

--- starter/client/src/components/Signup.js
+++ starter/client/src/components/Signup.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container'
+import TextField from "@material-ui/core/TextField";
+import { Button } from '@material-ui/core';
+import {
+  signupFormStyle,
+  textFieldStyle,
+  h1Style,
+  labelStyle,
+  buttonHolderStyle,
+  buttonStyle,
+} from '../styles/signupStyles';
+
+
 
 function Signup(props) {
   return (
     <>
-      <div>Hello World</div>
+      <CssBaseline />
+      <Container style = {{backgroundColor: '#f0f2f6', height: "100vh"}} maxWidth="large">
+        <h1 style={h1Style}>Welcome! Who are you?</h1>
+        <form style={signupFormStyle}>
+          <label style={labelStyle} htmlFor="email">Email</label>
+          <TextField
+            id='email'
+            type='email'
+            variant="outlined"
+            style={textFieldStyle}
+           />
+          <label style={labelStyle} htmlFor="password">Password</label>
+          <TextField
+            id='password'
+            type='password'
+            variant="outlined"
+            style={textFieldStyle}
+           />
+          <label style={labelStyle} htmlFor="firstName">First Name</label>
+          <TextField
+            id='firstName'
+            type='text'
+            variant="outlined"
+            style={textFieldStyle}
+           />
+          <label style={labelStyle} htmlFor="lastName">Last Name</label>
+          <TextField
+            id='lastName'
+            type='text'
+            variant="outlined"
+            style={textFieldStyle}
+           />
+           <div style={buttonHolderStyle}>
+            <Button variant="contained" style={buttonStyle}>Sign Up</Button>
+            <Button variant="contained" style={buttonStyle} href="/login">Login Instead</Button>
+           </div>
+        </form>
+      </Container>
     </>
   );
 }
---
+++ starter/client/src/styles/signupStyles.js
@@ -0,0 +1,39 @@
+export const signupFormStyle = {
+  margin: "auto",
+  width: "50%",
+  display: "flex",
+  flexDirection: "column",
+  padding: "50px 0 50px 0"
+}
+
+export const textFieldStyle = {
+  width: "40%",
+  margin: 'auto',
+  padding: '0px 0px 30px 0px',
+}
+
+export const h1Style = {
+  margin: "auto",
+  paddingTop: "5%",
+  width: "50%",
+  textAlign: "center",
+}
+
+export const labelStyle = {
+  margin: "auto",
+  width: "40%",
+}
+
+export const buttonHolderStyle = {
+  width: "50%",
+  margin: "auto",
+  display: "flex",
+  justifyContent: "space-between"
+}
+
+export const buttonStyle = {
+  width: "40%",
+  backgroundColor: "blue",
+  color: "white",
+  textAlign: "center",
+}
