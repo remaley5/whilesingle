49196a5360fbaa4418edca344a78cbd5497a1e79
Isaac <finken.isaac@gmail.com>
2020-10-28 11:45:03 -0500

    Added match percent route.

--- starter/client/src/components/fr_questions/FrContainer.js
+++ starter/client/src/components/fr_questions/FrContainer.js
@@ -9,6 +9,8 @@ export default function FrContainer({ view }) {
 
   const { userAnsweredFr, userUnansweredFr, matchAnsweredFr } = frContext;
 
+	// if user view - that means they want to edit their own responses
+	// render form
   if (view === "user") {
     const frArr = [...userAnsweredFr, ...userUnansweredFr];
     return (
@@ -17,7 +19,10 @@ export default function FrContainer({ view }) {
           <FrUserForm key={idx} frObj={frObj} />
         ))}
       </>
-    );
+		);
+		// if not user view - that means they want to view a match's profile
+		// or they want to view their own profile as their matches see it
+		// render view
   } else {
     const frArr = [...matchAnsweredFr];
     return (
