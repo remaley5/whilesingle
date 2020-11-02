4d836234dbfeb387d4789d00cd577f0632c2099f
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-02 09:52:35 -0500

    stuff

--- starter/client/src/components/messengerComponents/UserBox.js
+++ starter/client/src/components/messengerComponents/UserBox.js
@@ -4,11 +4,7 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import socketIOClient from "socket.io-client"
 
 // const ENDPOINT = "http://localhost:3000"
-<<<<<<< HEAD
-const endpoint = 8000;
-=======
 const endpoint = 8000//process.env.REACT_APP_ENDPOINT || 8000;
->>>>>>> c75b28521249f069feaa2368216210b0df3be583
 
 
 const UserBox = ({user, scrollDiv, setSelectedName, recipientId, setRecipientId, setMatch, setMessages, messages }) => {
