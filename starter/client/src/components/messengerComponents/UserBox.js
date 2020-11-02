af48a5b77eaf54c695d2f41b31aeee9aa97fae62
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-02 09:15:01 -0500

    stuff

--- starter/client/src/components/messengerComponents/UserBox.js
+++ starter/client/src/components/messengerComponents/UserBox.js
@@ -3,7 +3,7 @@ import '../../styles/messenger.css'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import socketIOClient from "socket.io-client"
 
-const ENDPOINT = "http://localhost:3000"
+// const ENDPOINT = "http://localhost:3000"
 const endpoint = process.env.REACT_APP_ENDPOINT || 8000;
 
 
